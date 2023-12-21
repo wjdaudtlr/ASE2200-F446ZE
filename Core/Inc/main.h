efad49586df5abc915d3b934205ec32b3075698e
wjdaudtlr <kim__aaa@naver.com>
2023-12-21 21:14:41 +0900

    implemented transmitting data string 1Hz

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -31,7 +31,7 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "ih_telemetry.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -47,10 +47,10 @@ void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOG_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
